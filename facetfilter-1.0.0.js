dbbe72a208bf64d753537f779be58f92a19d8c1a
Andrew Lowther <andy.lowther@mademedia.co.uk>
2013-02-05 10:51:34 +0000

    Adding filters that can be set as "single use"
    Signed-off-by: Andrew Lowther <andy.lowther@mademedia.co.uk>

--- facetfilter-1.0.0.js
+++ facetfilter-1.0.0.js
@@ -1,14 +1,17 @@
 (function($) {
 	
 	$.facetfilter = function( element, options ) {
+		var settings, facets, $element, element, init;
+
 		/**
 		 * Default plugin settings
 		 */
-		var settings = {
-			"version": "1.0.1", // Current plugin version
+		settings = {
+			"version": "1.0.2", // Current plugin version
 			"filters": null, // Selection of filters that can be applied
 			"anti_filters": null, // Deselection filters
 			"active_filters": {}, // Current object of active filters
+			"single_filters": [], // Define a list of filters that are single use
 			"ajax_url": null, // Ajax request URL
 			"url_options": {}, // Extra options to pass to the URL object (ex. is_ajax: 1)
 			"onsuccess": null, // Success callback
@@ -19,16 +22,16 @@
 		};
 		
 		// Set the facet as this
-		var facets = this;
+		facets = this;
 		
 		// Empty settings for now
 		facets.settings = {};
 		
-		var $element = $(element), // Our dom reference
+		$element = $(element), // Our dom reference
 			element = element; // The actual dom reference
 		
 		// Set up the plugin options
-		var init = function() {
+		init = function() {
 			facets.settings = $.extend({}, settings, options);
 			
 			$element.on("change", facets.settings.filters, facets.select);
@@ -37,6 +40,8 @@
 		
 		// Set up a request
 		facets.request = function( type ) {
+			var onsuccess, oncomplete, onerror, key, facet_data;
+
 			if( !facets.settings.ajax_url ) {
 				throw new Error("You must supply an ajax URL, bailing");
 			} else {
@@ -46,25 +51,19 @@
 					type = type.toUpperCase();
 				}
 				
-				if( facets.settings.onsuccess === null ) {
-					var onsuccess = function( response, textStatus, jqXHR ) {};
-				} else {
-					var onsuccess = facets.settings.onsuccess;
+				if( facets.settings.onsuccess !== null ) {
+					onsuccess = facets.settings.onsuccess;
 				}
 				
-				if( facets.settings.oncomplete === null ) {
-					var oncomplete = function( jqXHR, textStatus ) {};
-				} else {
-					var oncomplete = facets.settings.oncomplete;
+				if( facets.settings.oncomplete !== null ) {
+					oncomplete = facets.settings.oncomplete;
 				}
 				
-				if( facets.settings.onerror === null ) {
-					var onerror = function( jqXHR, textStatus, errorThrown ) {};
-				} else {
-					var onerror = facets.settings.onerror;
+				if( facets.settings.onerror !== null ) {
+					onerror = facets.settings.onerror;
 				}
 				
-				var key, facet_data = facets.settings.active_filters;
+				facet_data = facets.settings.active_filters;
 				// Add in options for the ajax request
 				for( key in facets.settings.url_options ) {
 					facet_data[key] = facets.settings.url_options[key];
@@ -91,11 +90,18 @@
 				
 				facets.settings.active_filters[facet] = {};
 			}
-			facets.settings.active_filters[facet][value] = value;
+
+			if( facets.isSingleFilter(facet) ) {
+				facets.settings.active_filters[facet] = value;
+			} else {
+				facets.settings.active_filters[facet][value] = value;
+			}
 		};
 		
 		// Remove facets from the filters
 		facets.removeFacet = function( facet, value ) {
+			var key, facet_count;
+
 			if( value !== null ) {
 				if( typeof facets.settings.active_filters[facet][value] !== 'undefined' ) {
 					delete facets.settings.active_filters[facet][value];
@@ -107,7 +113,7 @@
 			}
 			
 			// Check if there are no more facets in the list, remove it completely from the active facets
-			var key, facet_count = 0;
+			facet_count = 0;
 			for( key in facets.settings.active_filters[facet] ) {
 				facet_count++;
 			}
@@ -120,14 +126,13 @@
 		
 		// Select a facet
 		facets.select = function( e ) {
-			// This is currently a bit of a hack for the projects I'm currently working on, feel free to change/fix this as needed
-			if( parseInt( $(this).val() ) ) {
-				var facet_name = $(this).parent().parent().attr("id");
-				facets.addFacet( facet_name, $(this).val() );
-			}
+			var facet_name;
+
+			facet_name = $(this).parent().parent().attr("id");
+			facets.addFacet( facet_name, $(this).val() );
 			
 			if( facets.settings.callback !== null ) {
-				facets.settings.callback.apply(facets, ["addFacet"]);
+				facets.settings.callback.apply(facets, ["addFacet", facet_name, $(this).val()]);
 			}
 			
 			facets.request();
@@ -135,11 +140,13 @@
 		
 		// Deselect a facet
 		facets.deselect = function( e ) {
-			var facet_name = $(this).attr("name").replace(/\[(.*)\]/, "");
+			var facet_name;
+
+			facet_name = $(this).attr("name").replace(/\[(.*)\]/, "");
 			facets.removeFacet( facet_name, $(this).val() );
 			
 			if( facets.settings.callback !== null ) {
-				facets.settings.callback.apply(facets, ["removeFacet"]);
+				facets.settings.callback.apply(facets, ["removeFacet", facet_name]);
 			}
 			
 			facets.request();
@@ -147,15 +154,59 @@
 		
 		// URI Encode active facets
 		facets.serialize = function( obj, prefix ) {
-			var str = [];
+			var str, p, k;
+
+			str = [];
 			if( obj === undefined ) {
 				obj = facets.settings.active_filters;
 			}
-			for( var p in obj ) {
-				var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+			for( p in obj ) {
+				k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
 				str.push(typeof v == "object" ? facets.serialize(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v));
 			}
 			return str.join("&");
+		}
+
+		// Get the index of an array, or add it if it doesn't exist
+		facets.isSingleFilter = function( filter ) {
+			if (!Array.prototype.indexOf) {
+				Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+					"use strict";
+					if (this == null) {
+						throw new TypeError();
+					}
+					var t = Object(this);
+					var len = t.length >>> 0;
+					if (len === 0) {
+						return -1;
+					}
+					var n = 0;
+					if (arguments.length > 1) {
+						n = Number(arguments[1]);
+						if (n != n) { // shortcut for verifying if it's NaN
+							n = 0;
+						} else if (n != 0 && n != Infinity && n != -Infinity) {
+							n = (n > 0 || -1) * Math.floor(Math.abs(n));
+						}
+					}
+					if (n >= len) {
+						return -1;
+					}
+					var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+					for (; k < len; k++) {
+						if (k in t && t[k] === searchElement) {
+							return k;
+						}
+					}
+					return -1;
+				}
+			}
+
+			if( facets.settings.single_filters.indexOf(filter) ) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 		
 		facets.activeFilters = function() {
@@ -167,11 +218,14 @@
 	}
 	
 	$.fn.facetfilter = function( options ) {
+		var facetfilter, ident;
+
+		ident = "facetfilter";
 		return this.each(function() {
-			if( undefined === $(this).data('facetfilter') ) {
-				var facetfilter = new $.facetfilter(this, options);
+			if( undefined === $(this).data(ident) ) {
+				facetfilter = new $.facetfilter(this, options);
 				
-				$(this).data("facetfilter", facetfilter);
+				$(this).data(ident, facetfilter);
 			}
 		});
 	}
