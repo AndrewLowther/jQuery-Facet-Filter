5612ee5591855ff492b199f0f21a35b16917b47c
Andrew Lowther <andrew.lowther@mademedia.co.uk>
2013-07-25 17:14:49 +0100

    Bower/grintifying the project
    Signed-off-by: Andrew Lowther <andrew.lowther@mademedia.co.uk>

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/*global module:false, require:false */
+module.exports = function (grunt) {
+  // 'use strict';
+
+	// Load all grunt tasks.
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+        mangle: false,
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+        src: '<%= pkg.scripts.main %>',
+        dest: '<%= pkg.scripts.minified %>'
+      }
+		},
+    jshint: {
+      options: {
+        browser: true,
+        devel: true,
+        eqeqeq: false,
+        jquery: true,
+        smarttabs: true,
+        undef: true,
+        unused: false,
+        globals: {
+          TweenLite: false,
+          Power3: false,
+          Modernizr: false,
+          YT: false,
+          Util: true,
+          App: true
+        }
+      },
+      files: ['Gruntfile.js', '<%= pkg.scripts.main %>']
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['uglify', 'jshint']
+    }
+	});
+
+	grunt.registerTask('default', ['uglify', 'jshint']);
+};
