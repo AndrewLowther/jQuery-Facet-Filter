8d09b594182d4476b68f52df145b1a5eec069e41
Andrew Lowther <andrew.lowther@mademedia.co.uk>
2013-07-26 10:16:30 +0100

    Bumping version number
    Signed-off-by: Andrew Lowther <andrew.lowther@mademedia.co.uk>

--- facetfilter.js
+++ facetfilter.js
@@ -8,7 +8,7 @@
      * Default plugin settings
      */
     settings = {
-      'version': '1.0.2', // Current plugin version
+      'version': '1.0.3', // Current plugin version
       'filters': null, // Selection of filters that can be applied
       'anti_filters': null, // Deselection filters
       'active_filters': {}, // Current object of active filters
